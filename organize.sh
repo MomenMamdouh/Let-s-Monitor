1c50c455793ee75d8c739fcb47bb0b1428cfbf7e
MomenMamdouh <Momen.Mamdouh@gmail.com>
2023-12-21 01:50:05 +0200

    Update copyrights

--- organize.sh
+++ organize.sh
@@ -2,8 +2,8 @@
 
 ##########################################################################
 # Yet Another Monitor (YAMon)
-# Copyright (c) 2013-present Al Caughey
-# All rights reserved.
+# Copyright (c) 2013-2020 Al Caughey
+# Copyright (c) 2023-present Mo'men Mamdouh# All rights reserved.
 #
 #  This program organizes files in your data directory as per settings in 
 #  config.file

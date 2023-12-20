ac66618a6f5e3ac9f50c742e8d4cbe1f887772b9
MomenMamdouh <Momen.Mamdouh@gmail.com>
2023-12-21 01:52:24 +0200

    Update copyrights

--- setup3.4.7.sh
+++ setup3.4.7.sh
@@ -2,8 +2,8 @@
 
 ##########################################################################
 # Yet Another Monitor (YAMon)
-# Copyright (c) 2013-present Al Caughey
-# All rights reserved.
+# Copyright (c) 2013-2020 Al Caughey
+# Copyright (c) 2023-present Mo'men Mamdouh# All rights reserved.
 #
 # Script to help set baseline values in config.file for YAMon3.x
 #

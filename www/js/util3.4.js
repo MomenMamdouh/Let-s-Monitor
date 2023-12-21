0dd46a7298e7c2102fc3b97a2c31dd15c3f6c3ef
MomenMamdouh <Momen.Mamdouh@gmail.com>
2023-12-21 02:08:06 +0200

    Update copyrights

--- www/js/util3.4.js
+++ www/js/util3.4.js
@@ -3,7 +3,8 @@
 ##########################################################################
 #																		 #
 # Yet Another Monitor (YAMon)											 #
-# Copyright (c) 2013-present Al Caughey									 #
+# Copyright (c) 2013-2020 Al Caughey
+# Copyright (c) 2023-present Mo'men Mamdouh									 #
 # All rights reserved.													 #
 # See `yamon3.x.js` for more T&C's										 #
 #																		 #
